6b918181f659c2db6118c9669648527339026865
Vguilhen <vinicius.guilhen2013@gmail.com>
2023-04-18 16:43:44 -0300

    add singletonLazy

--- src/main/java/org/example/simgleton/SingletonLazy.java
+++ src/main/java/org/example/simgleton/SingletonLazy.java
@@ -2,5 +2,24 @@ package org.example.simgleton;
 
 public class SingletonLazy {
 
-    
+    //Criamos uma instância do prórpio singleton
+    private static SingletonLazy instance;
+
+    //Para garantir que ninguém externamente irá instanciar esse singleton? tornando-o privado;
+    private SingletonLazy(){
+        //deixando a chamada do contrutor da casse pai ex   plícito (opicional)
+        super();
+    }
+
+    //para garantir que essa instância vai ser exposta de alguma forma eu posso criar um método público
+
+    public static SingletonLazy getInstance(){
+        if (instance == null) {
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+
+
+
 }
