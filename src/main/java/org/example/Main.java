24092f9fb9069c10feed47745f06522d7aea87bf
Vguilhen <vinicius.guilhen2013@gmail.com>
2023-04-18 18:11:52 -0300

    testes no main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import org.example.simgleton.SingletonEager;
+import org.example.simgleton.SingletonLazy;
+import org.example.simgleton.SingletonLazyHolder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //testando os design patterns
+        //não é possível instanciar o lazy utlizando o "new", é preciso acessar pelo método get da classe
+        // SingletonLazy lazy = new SingletonLazy();
+        SingletonLazy lazy = SingletonLazy.getInstance();
+        //verificar o endereço de memória
+        System.out.println(lazy);
+        //se pegar uma nova instância ele deverá retornar a mesma ou seja o mesmo endereço de memória
+        lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstance();
+        //verificar o endereço de memória
+        System.out.println(eager);
+        //se pegar uma nova instância ele deverá retornar a mesma ou seja o mesmo endereço de memória
+        eager = SingletonEager.getInstance();
+        System.out.println(eager);
+
+        SingletonLazyHolder holder = SingletonLazyHolder.getInstance();
+        System.out.println(holder);
+        holder = SingletonLazyHolder.getInstance();
+        System.out.println(holder);
     }
 }
--- src/main/java/org/example/simgleton/SingletonLazyHolder.java
+++ src/main/java/org/example/simgleton/SingletonLazyHolder.java
@@ -1,2 +1,16 @@
-package org.example.simgleton;public class SingletonLazyHolder {
+package org.example.simgleton;
+
+public class SingletonLazyHolder {
+    //a classe holder só tem a função de encapsular o holder
+    public static class InstanceHolder {
+        public static SingletonLazyHolder instance = new SingletonLazyHolder();
+    }
+        private SingletonLazyHolder() {
+            super();
+        }
+
+    //não verifica se a instância é nula
+    public static SingletonLazyHolder getInstance(){
+        return InstanceHolder.instance;
+    }
 }
